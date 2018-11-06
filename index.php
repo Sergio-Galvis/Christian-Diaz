0a1ec0aee01d0d63c9008b74f33d2c19341ce89b
Sergio Galvis <sergioa.galvisv@gmail.com>
2018-11-05 19:25:26 -0500

    PHP files.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+Author: Bilden Software
+Author URL: http://bildensoftware.com
+ */
+include "app/config.php";
+include "app/detect.php";
+
+if ($page_name=='') {
+	include $browser_t.'/index.html';
+	}
+elseif ($page_name=='index.html') {
+	include $browser_t.'/index.html';
+	}
+elseif ($page_name=='contact-post.html') {
+	include 'app/contact.php';
+	}
+else
+	{
+		include $browser_t.'/404.html';
+	}
+
+?>
