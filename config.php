125593a795ede28d12d395a9d8d22814a6344ef4
Sergio Galvis <sergioa.galvisv@gmail.com>
2018-11-05 19:24:37 -0500

    PHP files.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Author: Bilden Software
+Author URL: http://bildensoftware.com
+*/
+$current_page_uri = $_SERVER['REQUEST_URI'];
+$part_url = explode("/", $current_page_uri);
+$page_name = end($part_url);
+$email_id = "contacto@doctorcristhiandiaz.com";
+?>
